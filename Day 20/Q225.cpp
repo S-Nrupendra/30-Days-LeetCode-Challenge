5838d8714db855bdcaf75a5108f8ff69797aacba
S-Nrupendra <snrupendra@gmail.com>
2025-09-08 17:43:10 +0530

    Add files via upload

---
+++ Day 20/Q225.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+public:
+    
+    queue<int> q1;
+    MyStack() {
+    }
+        
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+      int p=q1.size();
+        while(p>1){
+            q1.push(q1.front());
+            q1.pop();
+            p--;
+        } 
+        int x=q1.front();
+        q1.pop();
+        return x; 
+    }
+    int top() {
+        return q1.back();
+    }
+      
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+int main(){
+    return 0;
+}
