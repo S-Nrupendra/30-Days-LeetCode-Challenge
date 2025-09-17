1191adf99ea471521e9dd8b30539009ce0eaa246
S-Nrupendra <snrupendra@gmail.com>
2025-09-17 19:14:41 +0530

    Add files via upload

---
+++ Day 28/Q494.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if ((target + total) % 2 != 0 || abs(target) > total) return 0;
+
+        int subsetSum = (target + total) / 2;
+        vector<int> dp(subsetSum + 1, 0);
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = subsetSum; j >= num; --j) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[subsetSum];
+    }
+};
+
+int main(){
+    return 0;
+}
